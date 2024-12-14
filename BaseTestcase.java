5cb32deb0be4c8a027c3ce935b3b2b3357625173
Gopal <gopalgoyal488@gmail.com>
2024-12-14 20:39:06 +0530

    only one line change

--- BaseTestcase.java
+++ BaseTestcase.java
@@ -14,7 +14,7 @@ import org.testng.annotations.Test;
 
 
 public class BaseTestcase extends LoadableComponent<BaseTestcase> {
-
+// base test case
 	protected  WebDriver driver;
 	@BeforeMethod
     public  void setUp() {
